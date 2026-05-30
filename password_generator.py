ddfe6668f0c71c0cca07ecf0098f3d62b0d109cd
EmmeGierre <marcogia1512@gmail.com>
2026-05-30 19:50:15 +0200

    quasi terminato i due esempi di esecuzione password

--- password_generator.py
+++ password_generator.py
@@ -1,12 +1,6 @@
 import string
 import secrets
-
-#print(string.ascii_uppercase)
-#print(string.digits)
-#print(string.ascii_lowercase)
-#print(string.punctuation)
-
-
+from pathlib import Path
 
 def generate_password(
         length: int = 12,
@@ -17,21 +11,59 @@ def generate_password(
         avoid_ambiguous: bool = False,
 ) -> str:
 
-    alfabeto = ""
+    alphabet = ""
     if use_upper:
-        alfabeto += string.ascii_uppercase
+        alphabet += string.ascii_uppercase
     if use_lower:
-        alfabeto += string.ascii_lowercase
+        alphabet += string.ascii_lowercase
     if use_digits:
-        alfabeto += string.digits
+        alphabet += string.digits
     if use_symbols:
-        alfabeto += string.punctuation
+        alphabet += string.punctuation
     if avoid_ambiguous:
-        alfabeto = "".join(n for n in alfabeto if n not in "Il1O0")
+        alphabet = "".join(n for n in alphabet if n not in "Il1O0")
 
-    password = ""
-    for _ in range(length):
-        character = secrets.choice(alfabeto)
-        password += character
+    if not alphabet:
+        raise ValueError("Empty alphabet")
+
+    set_true = use_upper + use_lower + use_digits + use_symbols
+
+    if length < set_true:
+        raise ValueError("Increase length your password")
+
+    password = []
+    if use_upper:
+        password.append(secrets.choice(string.ascii_uppercase))
+    if use_lower:
+        password.append(secrets.choice(string.ascii_lowercase))
+    if use_digits:
+        password.append(secrets.choice(string.digits))
+    if use_symbols:
+        password.append(secrets.choice(string.punctuation))
+    for _ in range(length - set_true):
+        password.append(secrets.choice(alphabet))
+    secrets.SystemRandom().shuffle(password)
+    password = "".join(password)
     return password
+
+if __name__ == "__main__":
+    print("=== Password generator ===")
+    print("1) Profilo default (12 caratteri, tutti i set, visualizza a schermo")
+    print("2) Profilo custom")
+    choice = input("Scelta: ")
+    if choice == "1":
+        print(f"Password generata: {generate_password()}")
+    elif choice == "2":
+        use_upper = input("Maiuscole? (y/n) [y]: ") == "y"
+        use_lower = input("Minuscole? (y/n) [y]: ") == "y"
+        use_digits = input("Cifre? (y/n) [y]: ") == "y"
+        use_symbols = input("Simboli? (y/n) [y]: ") == "y"
+        length = int(input("Lunghezza [12]: "))
+        avoid_ambiguous = input("Evitare caratteri ambigui? (y/n) [y]: ") == "y"
+        save_password = input("Salvare su file? (y/n) [y]: ") == "y"
+        print("Path del file: ~/passwords/sith_lord.txt")
+        password_generated = generate_password(length=length, use_upper=use_upper, use_lower=use_lower, use_digits=use_digits, use_symbols=use_symbols, avoid_ambiguous=avoid_ambiguous)
+        print(f"Password generata: {password_generated}")
+        print(input("Salvata in /Users/marco/passwords/sith_lord.txt"))
+
 
